073ed769d31585ef329718b180fb28e454aec0b7
LiDaoKuan <2112183373@qq.com>
2025-11-25 23:34:54 +0800

    finish DNSCache

---
+++ src/util/DnsUtil.h
@@ -0,0 +1,14 @@
+//
+// Created by ldk on 10/31/25.
+//
+
+#ifndef MYWORKFLOW_DNSUTIL_H
+#define MYWORKFLOW_DNSUTIL_H
+
+
+class DnsUtil
+{
+};
+
+
+#endif //MYWORKFLOW_DNSUTIL_H
---
+++ src/util/LRUCache.h
@@ -0,0 +1,8 @@
+//
+// Created by ldk on 10/31/25.
+//
+
+#ifndef MYWORKFLOW_LRUCACHE_H
+#define MYWORKFLOW_LRUCACHE_H
+
+#endif //MYWORKFLOW_LRUCACHE_H
